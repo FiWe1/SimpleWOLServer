5796acd7812a544cf79c3b72bb1451c8b26a74fe
FiWe1 <filipweissen@gmail.com>
2025-11-12 16:07:09 +0100

    routing remake

--- wol_server.py
+++ wol_server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 import wol
 from config import PC_MAC, PORT
@@ -11,10 +11,26 @@ app = Flask(__name__)
 def home():
     return "Go to <a href='/wake'>/wake</a> to WOL your device."
 
+
 @app.route("/wake")
 def wake():
-    wol.wol(PC_MAC)  # Change this to your desktop’s Ethernet MAC
-    return "Wake-on-LAN packet sent!"
+    try:
+        wol.wol(PC_MAC, times=3)  # Edit your desktop’s Ethernet MAC in config.py
+    except Exception:
+        return redirect(url_for("failure"))
+    
+    return redirect(url_for("success"))
+
+
+@app.route("/success")
+def success():
+    return "Wake-on-LAN packet sent!\n" + "<a href='/wake'>Click</a> to resend."
+
+
+@app.route("/failure")
+def failure():
+    return "Failed to send WOL packet. Try again: \n" + "<a href='/wake'>Click</a> to resend."
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
