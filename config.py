e84f9f1fa66da9b001dd9b7554c1d869b1bc646b
FiWe1 <filipweissen@gmail.com>
2025-11-12 16:13:48 +0100

    config edit

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
 # modify these constants
 
-# LAN broadcast IP address
-#LAN_BROADCAST = "<broadcast>"
-LAN_BROADCAST = "169.254.255.255"
+# Your LAN broadcast IP address
+LAN_BROADCAST = "192.168.255.255"
 
 # mac address of PC to be waken by LAN
-PC_MAC = b"\x4B\x2E\x00\xEB\xB4\x3D" #h310m
-
-
+PC_MAC = b"\x4B\x2E\x00\xEB\xB4\x3D"
 
 
 # server port
